661203ad9b696eef3257a100863240053ac31f9e
Muhammad Rafly Alfayed <mralfayed@gmail.com>
2024-03-23 23:20:33 +0700

    make revision on models

--- app/models/email.py
+++ app/models/email.py
@@ -1,13 +1,8 @@
-# app/models/email.py
-from datetime import datetime
 from app import db
 
 class Email(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer)
-    email_subject = db.Column(db.String(255))
-    email_content = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Email {self.id}>'
+    event_id = db.Column(db.Integer, nullable=False)
+    email_subject = db.Column(db.String(255), nullable=False)
+    email_content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
