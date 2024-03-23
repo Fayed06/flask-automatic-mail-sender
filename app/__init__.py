7f278d625aa70c4b3cdd06bbc1a7c9b93de45e1e
Muhammad Rafly Alfayed <mralfayed@gmail.com>
2024-03-23 23:19:31 +0700

    make the init.py more clean

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,11 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-from apscheduler.schedulers.background import BackgroundScheduler
-from dotenv import load_dotenv
+from sqlalchemy.exc import IntegrityError
 
-load_dotenv()  # Load variables from .env file
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Flask-Mail configuration
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS').lower() == 'true'
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-
-mail = Mail(app)
-
-scheduler = BackgroundScheduler()
-scheduler.start()
-
+app = Flask(__name__, template_folder='views/templates')
+app.config.from_pyfile('../config.py')
 db = SQLAlchemy(app)
 
-from app import models, controllers
-
-db.create_all()
+from app.controllers.email_controller import email_bp
+app.register_blueprint(email_bp)
